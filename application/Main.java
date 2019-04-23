ac436d1941865cb97512077c74efa23d142d12ba
Ben Zerler <zerler@wisc.edu>
2019-04-22 21:04:01 -0500

    starter gui with vbox, hbox, and buttons

--- application/Main.java
+++ application/Main.java
@@ -3,8 +3,11 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -15,8 +18,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			Label quizGenerator = new Label("Quiz Generator");
-			root.setTop(quizGenerator);
+			createHomepage(root, scene);
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -25,6 +27,22 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void createHomepage(BorderPane root, Scene scene) {
+	  Label quizGenerator = new Label("Quiz Generator");
+      root.setTop(quizGenerator);
+      
+      Button addQuestion = new Button("Add Question");
+      Button loadQuestions = new Button("Load Questions");
+      Button saveQuestions = new Button("Save Questions");
+      
+      Label numberOfQuestions = new Label("Number of Questions: ");
+      
+      HBox topBox = new HBox(addQuestion, loadQuestions, saveQuestions);
+      VBox mainVBox = new VBox(topBox, numberOfQuestions);
+      
+      root.setCenter(mainVBox);
 	}
 	
 	public static void main(String[] args) {
