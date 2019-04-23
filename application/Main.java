4c2bbdb2d8a233809bf98b3b3301d400fc08d232
Ben Zerler <zerler@wisc.edu>
2019-04-22 21:33:09 -0500

    more complete home gui

--- application/Main.java
+++ application/Main.java
@@ -1,10 +1,15 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -36,13 +41,25 @@ public class Main extends Application {
       Button addQuestion = new Button("Add Question");
       Button loadQuestions = new Button("Load Questions");
       Button saveQuestions = new Button("Save Questions");
+      Label numberOfQuestionsLoaded = new Label("Number of Questions Loaded: ");
       
-      Label numberOfQuestions = new Label("Number of Questions: ");
+      ListView<CheckBox> listView = new ListView<CheckBox>();
+      ObservableList<CheckBox> topics = FXCollections.observableArrayList();
+      
+      topics.add(new CheckBox("hash table"));
+      topics.add(new CheckBox("linux"));
+      topics.add(new CheckBox("tree"));
+      listView.setItems(topics);
       
       HBox topBox = new HBox(addQuestion, loadQuestions, saveQuestions);
-      VBox mainVBox = new VBox(topBox, numberOfQuestions);
-      
+      VBox mainVBox = new VBox(topBox, numberOfQuestionsLoaded, listView);
       root.setCenter(mainVBox);
+      
+      Button startButton = new Button("Start");
+      Label desiredQuestions = new Label("Desired Number of Questions: ");
+      TextField numQuestions = new TextField();
+      HBox bottomBox = new HBox(startButton, desiredQuestions, numQuestions);
+      root.setBottom(bottomBox);
 	}
 	
 	public static void main(String[] args) {
