ca7f434e55e07865f8a1694887721feb223073c8
Ben Zerler <zerler@wisc.edu>
2019-04-26 11:57:59 -0500

    created Quiz class

---
+++ application/Quiz.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Quiz {
+//  ArrayList<Question> questions;
+  int answersCorrect;
+  int answersIncorrect;
+  double score;
+  
+  public Quiz (String[] topics) {
+    answersCorrect = 0;
+    answersIncorrect = 0;
+    score = 0.0;
+  }
+}
