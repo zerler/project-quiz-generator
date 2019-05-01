0246982a167083ea25368f2aa94c62a76ea3314f
Ben Zerler <zerler@wisc.edu>
2019-04-30 21:15:29 -0500

    added comments to Quiz.java

--- application/Quiz.java
+++ application/Quiz.java
@@ -2,21 +2,31 @@ package application;
 
 import java.util.ArrayList;
 
+/**
+ * Quiz class to store questions and score for each quiz.
+ * 
+ */
 public class Quiz {
-  ArrayList<Question> questions;
-  int answersCorrect;
-  int answersIncorrect;
-  double score;
+  ArrayList<Question> questions; //all questions to display for this quiz
+  int answersCorrect; //# of questions answered correctly
+  int answersIncorrect; //# of questions answered incorrectly
+  double score; //calculated score
   
+  /**
+   * Constructor of a quiz, initialize all fields
+   * @param questions - the questions to put on the quiz
+   */
   public Quiz (ArrayList<Question> questions) {
     answersCorrect = 0;
     answersIncorrect = 0;
     score = 0.0;
-    
     this.questions = questions;
-    
   }
   
+  /**
+   * Calculate score to put on the result GUI screen.
+   * @return - a percent of questions answered correctly
+   */
   public double calculateScore() {
     for (Question question : questions) {
       if (question.isCorrect)
@@ -29,20 +39,36 @@ public class Quiz {
     return score;
   }
   
+  /**
+   * Returns number of questions answered correctly
+   * @return answersCorrect
+   */
   public int getNumberAnswersCorrect() {
 	  return answersCorrect;
   }
   
+  /**
+   * Returns the score on this quiz.
+   * @return score
+   */
   public double getScore() {
+      this.calculateScore();
 	  return score;
   }
   
+  /**
+   * Get the number of questions on this quiz.
+   * @return number of questions
+   */
   public int getNumQuestions() {
 	  return questions.size();
   }
   
+  /**
+   * Return an arraylist of all of the questions on the quiz.
+   * @return all questions
+   */
   public ArrayList<Question> getQuestions(){
 	  return questions;
   }
-  
 }
